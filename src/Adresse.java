911072f8343a0274939a8e91472d6e97c3861951
Théo/ <80842242+Theocyg@users.noreply.github.com>
2023-01-31 20:34:57 +0100

    Update Adresse.java

--- src/Adresse.java
+++ src/Adresse.java
@@ -4,8 +4,8 @@ private String ville;
 private String codePostal;
 public Adresse(String rue, String ville, String codePostal){
         this.rue = validate(rue, 100);
-        this.ville = validate(ville, 50);
-        this.codePostal = validate(codePostal, 10);
+        this.ville = validate(ville, 20);
+        this.codePostal = validate(codePostal, 5);
         }
 private String validate (String chaine, Integer taille){
         chaine = chaine.substring(0, Math.min(chaine.length(), taille));
