55e09e7fe37abeb3791888a34ed6054a19c1d34f
Théo/ <80842242+Theocyg@users.noreply.github.com>
2023-02-01 16:57:38 +0100

    Update Architecte.java

--- src/Architecte.java
+++ src/Architecte.java
@@ -1,8 +1,63 @@
 import java.util.*;
- class Architecte {
+import java.lang.String;
+ class Architecte extends Personne{
 
+     public Adresse adresse;
+     public String telephone;
      private String conseilRegional;
      private boolean inscriptionOrdre;
+     private String telephoneValide;
+     public void Maj_conseilRegional{
+         System.out.println(nom+ "/t "+prenom+"/t "+conseilRegional+"/t "+telephone+"/t "+inscriptionOrdre+"/t ");
+     }
+
+     if(telephoneValide(telephone)){
+         this.telephone=telephone;
+     }
+        else{
+         this.telephone="";
+         Maj_conseilRegional();
+     }
+     public Architecte(String nom, String prenom, String conseilRegional, boolean inscriptionOrdre, Adresse adresse, String telephone){
+         super(nom, prenom);
+         this.conseilRegional=conseilRegional;
+         this.inscriptionOrdre=inscriptionOrdre;
+         this.adresse = adresse;
+
+         private boolean telephoneValide(String telephone){
+             if (telephone.length() == 10 && telephone.equals(telephone)) {
+                 return true;
+             }
+             else {
+                 return false;
+             }
+         }
+
+         public void Maj_conseilRegional(){
+             if (inscriptionOrdre) {
+                 conseilRegional = conseilRegional + " - Inscrit au conseil de l'ordre";
+             } else {
+                 conseilRegional = conseilRegional + " - Non inscrit au conseil de l'ordre";
+             }
+
+         }
+
+         public String toString (){
+             String chaine = "";
+             chaine = chaine + this.nom + " " + this.prenom + " ";
+             chaine = chaine + this.conseilRegional + " ";
+             chaine = chaine + this.adresse + " ";
+             chaine = chaine + this.telephone + " ";
+             if (inscriptionOrdre){
+                 chaine = chaine + "Inscrit" + " ";
+             }
+             else{
+                 chaine = chaine + "Non inscrit" + " ";
+             }
+             return chaine;
+         }
+     }
+
 
      public void setConseilRegional(String conseilRegional) {
          this.conseilRegional = conseilRegional;
@@ -11,51 +66,17 @@ import java.util.*;
      public String getConseilRegional() {
          return conseilRegional;
      }
+     public String getTelephone() {
+         return telephone;
+     }
 
-     public String Architecte(String nom, String prenom, String civilite, String mail, String conseilRegional, boolean inscriptionOrdre, Adresse adresse, String telephone){
-        super(nom, prenom, civilite, mail);
-        this.conseilRegional=conseilRegional;
-        this.inscriptionOrdre=inscriptionOrdre;
-        this.adresse = adresse;
-        if(telephoneValide(telephone)){
-            this.telephone=telephone;
-        }
-        else{
-            this.telephone="";
-            Maj_conseilRegional();
-        }
-        private boolean telephoneValide(String telephone){
-            if (telephone.length() == 10 && telephone.equals(telephone)) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+     public void setTelephone(String telephone) {
+         this.telephone = telephone;
+     }
 
-        public void Maj_conseilRegional(){
-            if (inscriptionOrdre) {
-                conseilRegional = conseilRegional + " - Inscrit au conseil de l'ordre";
-            } else {
-                conseilRegional = conseilRegional + " - Non inscrit au conseil de l'ordre";
-            }
 
-        }
-
-        public String toString (){
-            String chaine = "";
-            chaine = chaine + this.nom + " " + this.prenom + " ";
-            chaine = chaine + this.civilite+""+this.mail+"";
-            chaine = chaine + this.conseilRegional + " ";
-            chaine = chaine + this.adresse + " ";
-            chaine = chaine + this.telephone + " ";
-            if (inscriptionOrdre){
-                chaine = chaine + "Inscrit" + " ";
-            }
-            else{
-                chaine = chaine + "Non inscrit" + " ";
-            }
-            return chaine;
-        }
-    }
-}
+     @Override
+     public String toString() {
+         return null;
+     }
+ }
