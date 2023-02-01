fcbde41b278c096afd27c4bf6e3a2ecad2452337
Théo/ <80842242+Theocyg@users.noreply.github.com>
2023-02-01 16:22:05 +0100

    Update Deposant.java

--- src/Deposant.java
+++ src/Deposant.java
@@ -1,18 +1,41 @@
-import Personne;
-
-public class Deposant extends Personne {
+import java.util.*;
+class Deposant extends Personne{
     private String civilite;
     private String mail;
-    private String adresse;
-
-    public Deposant (String civilite, String mail, String adresse){
+    private Adresse adresse;
+    public Deposant(String nom, String prenom, String civilite, String mail, Adresse adresse) {
         super(nom, prenom);
-        this.civilite= validate(civilite,10);
-        this.mail=validate(mail,10);
-        this.adresse=adresse;
+        this.civilite = validate(civilite, 10);
+        this.mail = validate(mail, 100);
+        this.adresse = adresse;
     }
+
+    public String getCivilite() {
+        return civilite;
+    }
+
+    public void setCivilite(String civilite) {
+        this.civilite = civilite;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
     @Override
-    public String toString(){
-        return "Deposant\ncivilite=" + civilite + ", mail=" + mail + ", adresse=" + adresse + ", nom=" +nom+ ", prenom=" +prenom;
+    public String toString() {
+        return "Deposant civilite=" + civilite + ", mail=" + mail + ", adresse=" + adresse + ", nom=" + nom + ", prenom=" + prenom +"";
     }
 }
